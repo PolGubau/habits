51029765eea2bb84da4fdee238faea153fa4780c
Pol Gubau <pol.gubau@meona.de>
2023-01-31 22:51:08 +0100

    Pol; MW-

--- src/components/Forms/Food/utils/initialState.ts
+++
@@ -1,8 +0,0 @@
-export const initialNewFoodState = {
-  name: "",
-  location: "",
-  date: new Date().toISOString().slice(0, 10),
-  time: new Date().toISOString().slice(11, 16),
-  ingredients: [""],
-};
-
