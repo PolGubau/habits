29c32b360924657e0ca61309820eaaec452f0cb6
Pol Gubau <pol.gubau@meona.de>
2023-01-31 22:58:30 +0100

    Pol; MW-

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,46 +2,40 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   root: true,
 
   extends: [
-    'plugin:@next/next/recommended',
-    'plugin:react/recommended',"eslint:recommended",
-    'standard-with-typescript'
+    "plugin:@next/next/recommended",
+    "plugin:react/recommended",
+    "eslint:recommended",
   ],
-  
+
   parser: `@typescript-eslint/parser`,
   parserOptions: {
-      "ecmaFeatures": {
-      "jsx": true
+    ecmaFeatures: {
+      jsx: true,
     },
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ["./tsconfig.json"],
     ecmaVersion: 12,
-    sourceType: 'module'
-    
+    sourceType: "module",
   },
-  
-  plugins: [
-    'react'
-  ],
-  
+
+  plugins: ["react"],
+
   rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
     "dot-notation": 0,
     "@typescript-eslint/no-throw-literal": ["error"],
-    '@typescript-eslint/no-unnecessary-type-assertion': 2,
+    "@typescript-eslint/no-unnecessary-type-assertion": 2,
     "@typescript-eslint/dot-notation": "warn",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/strict-boolean-expressions": "off",
     "@typescript-eslint/no-misused-promises": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/no-floating-promises": "off",
-    
   },
-  
-  
-}
+};
